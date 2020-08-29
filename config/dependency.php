eae12fadfcf35fa21fd035ba8d21f506d47d7e5a
BibaltiK <bibaltik@exdrals.de>
2020-08-29 21:40:44 +0200

    -

--- config/dependency.php
+++ config/dependency.php
@@ -1,6 +1,6 @@
 <?php
 return [
     'Exdrals\Excidia\Component\Router\Router' => [
-        'dependencies' => ['Symfony\Component\HttpFoundation\Request', 'Symfony\Component\HttpFoundation\Request']
+        'dependencies' => ['Symfony\Component\HttpFoundation\Request']
     ]
 ];
