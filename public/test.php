a14ad0bcbb4c0cfccefd8e5cd8d8d33b62025513
BibaltiK <bibaltik@exdrals.de>
2020-09-11 19:45:32 +0200

    -

--- public/test.php
+++ public/test.php
@@ -1,2 +1,29 @@
-<?php
-phpinfo();
+<html>
+    <head>
+        <style>
+            body {
+                background-color: #303030;
+            }
+            .div-div-a-1{
+    position: relative;
+}
+.div-a-1{
+    text-decoration: none;
+}
+.div-a-span-4{
+    font: normal normal normal 15px/1.4em proxima-n-w01-reg,sans-serif;
+    font: normal normal normal 16px/20px proxima-n-w01-reg,sans-serif;
+    color: #FFFFFF;
+    text-align: center;
+    margin: auto;
+}
+        </style>
+    </head>    
+    <body>
+<div class="div-div-a-1">
+    <a href="index.php?page=Hinweise-zu-Cookies" class="div-a-1">
+        <span class="div-a-span-4">Hinweise zu Cookies</span>
+    </a>
+</div>
+        </body>
+</html>
