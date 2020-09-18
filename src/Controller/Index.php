cea6c663afaf003aa90956d19d678f9955d57805
BibaltiK <bibaltik@exdrals.de>
2020-09-18 21:45:10 +0200

    style change

--- src/Controller/Index.php
+++ src/Controller/Index.php
@@ -3,13 +3,32 @@ declare(strict_types=1);
 
 namespace Exdrals\Bugebo\Controller;
 
+use Exdrals\Excidia\Component\Template\Template;
+use Symfony\Component\HttpFoundation\Request;
 use Exdrals\Bugebo\Controller\AbstractController;
+use Exdrals\Bugebo\Controller\Auth;
 
 class Index extends AbstractController
 {
     
+    protected Auth $auth;
+
+    public function __construct(Template $template, Request $request, Auth $auth) 
+    {
+        $this->auth = $auth;
+        parent::__construct($template, $request);
+    }
     public function index() : ?string
     {                                                  
-        return 'Index';
+        return null;
+    }
+    
+    public function addLoggedInBar()
+    {
+        if (!$this->auth->isLoggedin())
+        {
+            return null;
+        }
+        return $this->template->render('partials/navigation_loggedin_bar');
     }
 }
