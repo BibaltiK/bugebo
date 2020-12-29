89593aa72c5eb155994e7b296e1f7063e7e4e28a
BibaltiK <bibaltik@exdrals.de>
2020-12-29 21:24:32 +0100

    refactor

--- src/Component/Router/Router.php
+++ src/Component/Router/Router.php
@@ -4,67 +4,61 @@ declare(strict_types=1);
 
 namespace Exdrals\Excidia\Component\Router;
 
-use Exdrals\Excidia\Component\Exception\{FileNotFoundException, 
-                                         RouteNotFoundException,
-                                         UnexpectedContentException
-    };
+use Exdrals\Excidia\Component\Exception\RouteNotFoundException;
 use Symfony\Component\HttpFoundation\Request;
+use function array_key_exists;
+use function array_key_last;
+use function count;
+use function explode;
+use function preg_match;
+use function rtrim;
 
-class Router 
+class Router
 {
-    protected ?array $routes = [];
-    protected Request $request;
+    protected array $routes = [];
     protected string $requestURI;
 
-    public function __construct(Request $request) 
+    public function __construct(protected Request $request)
     {
-        $this->routes = [];
-        $this->request = $request;
         $this->requestURI = $this->getModifiedRequestURI();
     }
 
-    public function setRoutes(array $routeConfig) : void
+    public function setRoutes(array $routeConfig): void
     {
         $this->routes = $routeConfig;
     }
 
-    public function match() : array
-    {               
-        foreach ($this->routes as $route => $routeInfo) 
-        {                        
+    public function match(): array
+    {
+        foreach ($this->routes as $route => $routeInfo) {
             $matches = [];
             $regEx = $this->getRegEx($routeInfo['method'], $routeInfo['path']);
-            
-            if (!preg_match($regEx, $this->requestURI, $matches)) 
-            {
-                continue;
-            }                        
-            
-            if (array_key_exists('params', $routeInfo))
-            {
 
-                $methodParams = explode('/', rtrim($matches[array_key_last($matches)],'/'));
-                if(count($routeInfo['params']) != count($methodParams))
-                {
-                    throw new RouteNotFoundException(sprintf('No matching route found for: <b>%s</b>',$this->requestURI));
+            if(!preg_match($regEx, $this->requestURI, $matches)) {
+                continue;
+            }
+
+            if(array_key_exists('params', $routeInfo)) {
+                $methodParams = explode('/', rtrim($matches[array_key_last($matches)], '/'));
+                if(count($routeInfo['params']) != count($methodParams)) {
+                    throw new RouteNotFoundException(sprintf('No matching route found for: <b>%s</b>', $this->requestURI));
                 }
                 $routeInfo['params'] = $methodParams;
-
             }
             return $routeInfo;
         }
-        throw new RouteNotFoundException(sprintf('No matching route found for: <b>%s</b>',$this->requestURI));
-    }
-    
-    protected function getModifiedRequestURI() : string
-    {
-        return $this->request->getMethod().'_'.$this->request->getRequestUri();
+        throw new RouteNotFoundException(sprintf('No matching route found for: <b>%s</b>', $this->requestURI));
     }
 
-    protected function getRegEx(string $method, string $route) : string
+    protected function getModifiedRequestURI(): string
     {
-        $regEx = '('.$method.')_'.$route;
+        return $this->request->getMethod() . '_' . $this->request->getRequestUri();
+    }
+
+    protected function getRegEx(string $method, string $route): string
+    {
+        $regEx = '(' . $method . ')_' . $route;
         $regEx = "~^$regEx/?$~i";
         return $regEx;
-    }    
+    }
 }
